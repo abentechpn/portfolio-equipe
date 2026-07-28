3bfe292c22db53a0d9853c3e5d49c69d2d1c5f51
abentechpn <abentech.pn@gmail.com>
2026-07-28 11:30:37 -0400

    Mise en place de React Router v6 et structure des pages

---
+++ client/src/pages/Equipe.jsx
@@ -0,0 +1,10 @@
+function Equipe() {
+  return (
+    <div>
+      <h1>Équipe</h1>
+      <p>Présentation des membres de l'équipe.</p>
+    </div>
+  );
+}
+
+export default Equipe;
---
+++ client/src/pages/Projets.jsx
@@ -0,0 +1,10 @@
+function Projets() {
+  return (
+    <div>
+      <h1>Projets</h1>
+      <p>Galerie des projets de l'équipe.</p>
+    </div>
+  );
+}
+
+export default Projets;
