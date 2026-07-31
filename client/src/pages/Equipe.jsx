3db2fd10a2c3df01c21f5cc35ccabd5f107fa2fe
abentechpn <abentech.pn@gmail.com>
2026-07-30 21:20:59 -0400

    Ajout du Context API langue et des statistiques GitHub

--- client/src/pages/Equipe.jsx
+++ client/src/pages/Equipe.jsx
@@ -1,8 +1,11 @@
+import GithubStats from '../components/GithubStats';
+
 export default function Equipe() {
   return (
     <div>
       <h1>Notre Équipe</h1>
       <p>Présentation des membres de l'équipe.</p>
+      <GithubStats username="abentechpn" />
     </div>
   );
 }
